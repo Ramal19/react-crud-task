43f29961b79a1f07517ecfb154ad186743a1d326
Ramal19 <ld4xz51vd@code.edu.az>
2024-05-16 08:40:10 +0400

    react-task

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import './App.css'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    
+    fetch("http://localhost:8000/users")
+    .then(res => res.json())
+    .then(data=>setData(data))
+  }, [])
+
+  const deleteData = function (id) {
+    axios.delete(`http://localhost:8000/users/${id}`)
+  }
+
+  console.log(data);
+  
+
+  return (
+    <>
+      <table border={2}>
+          <thead>
+              <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>E-mail</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map(item=>{
+                return(
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.age}</td>
+                  <td>{item.email}</td>
+                  <td className='deleteBtn'><button onClick={()=>deleteData(item.id)}>X</button></td>
+                </tr>
+                )
+              })
+            }
+          </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
+
+
